c014f0221fb11fe1327707595177b275ee43f720
Marek Zelinka <mzelinka17@gmail.com>
2024-06-17 10:48:12 +0200

    setup

--- app/root.tsx
+++ app/root.tsx
@@ -9,14 +9,14 @@ import "./tailwind.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full antialiased">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="h-full">
         {children}
         <ScrollRestoration />
         <Scripts />
